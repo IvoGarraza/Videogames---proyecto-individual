8df773aa15ad1a5ce94f3b66cc07ef955dc5f61f
ivo garraza <ivogarrachip@gmail.com>
2022-06-13 19:48:44 -0300

    controllers

--- api/src/controllers/videogames.js
+++ api/src/controllers/videogames.js
@@ -6,8 +6,17 @@ const getInfo = async () => {
     try {
         const ApiInfo = axios.get(`https://api.rawg.io/api/games?key=${API_KEY}`)
         console.log(ApiInfo)
+        const ApiInfoResults = ApiInfo.data.results.map( async (obj) => ) 
     } catch (error) {
         console.log(error)
         return error
     }
 }
+
+/* const objVideogame = (videogame) =>{
+    const obj = {
+        id: videogame.id
+        name: videogame.name,
+        description: videogame.
+    }
+} */
